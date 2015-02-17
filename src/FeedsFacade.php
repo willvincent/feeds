3d55306608e63f5e81336fdf4989d2b9142c8173
Will Vincent <will@willvincent.com>
2015-02-17 02:11:43 -0600

    Remove facade

--- src/FeedsFacade.php
+++
@@ -1,9 +0,0 @@
-<?php namespace willvincent\Feeds;
-
-use Illuminate\Support\Facades\Facade;
-
-class Feed extends Facade {
-
-    protected static function getFacadeAccessor() { return 'Feeds'; }
-
-}
