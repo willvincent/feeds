f18c53c6dcd6d7a024893fbdc49df7b6c88e524b
ahmed <ahmedfawzy.dev@gmail.com>
2015-07-20 12:27:56 +0200

    add disable strip tags support

--- src/config/feeds.php
+++ src/config/feeds.php
@@ -1,6 +1,26 @@
 <?php
 
 return [
+  /*
+  |--------------------------------------------------------------------------
+  | Strip Html Tags Disabled
+  |--------------------------------------------------------------------------
+  |
+  |
+  |
+  */
+  'strip_html_tags.disabled'=> false,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Striped Html Tags
+  |--------------------------------------------------------------------------
+  |
+  |
+  |
+  */
+//	'base', 'blink', 'body', 'doctype', 'embed', 'font', 'form', 'frame', 'frameset', 'html', 'iframe', 'input', 'marquee', 'meta', 'noscript', 'object', 'param', 'script', 'style'
+  'strip_html_tags.tags'=> [ 'base', 'blink', 'body', 'doctype', 'embed', 'font', 'form', 'frame', 'frameset', 'html', 'iframe', 'input', 'marquee', 'meta', 'noscript', 'object', 'param', 'script', 'style'],
 
   /*
   |--------------------------------------------------------------------------
