3c463fc176532ea797221651900b0849b50e6006
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-10-05 19:38:14 +0200

    reformat config and fix some typos

--- src/config/feeds.php
+++ src/config/feeds.php
@@ -2,98 +2,103 @@
 
 return [
 
-  /*
-  |--------------------------------------------------------------------------
-  | Cache Location
-  |--------------------------------------------------------------------------
-  |
-  | Filesystem path to use for caching, the default should be acceptable in
-  | most cases.
-  |
-  */
-  'cache.location' => storage_path() . '/framework/cache',
-
-  /*
-  |--------------------------------------------------------------------------
-  | Cache Life
-  |--------------------------------------------------------------------------
-  |
-  | Life of cache, in seconds
-  |
-  */
-  'cache.life'     => 3600,
-
-  /*
-  |--------------------------------------------------------------------------
-  | Cache Disabled
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-  'cache.disabled' => false,
     /*
-  |--------------------------------------------------------------------------
-  | Disable Check for SSL certificates (enable for self signed certificates)
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-  'ssl_check.disabled' => false,
-  /*
-  |--------------------------------------------------------------------------
-  | Strip Html Tags Disabled
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-  'strip_html_tags.disabled'=> false,
+    |--------------------------------------------------------------------------
+    | Cache Location
+    |--------------------------------------------------------------------------
+    |
+    | Filesystem path to use for caching, the default should be acceptable in
+    | most cases.
+    |
+    */
+    'cache.location'           => storage_path('framework/cache'),
 
-  /*
-  |--------------------------------------------------------------------------
-  | Striped Html Tags
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-  // 'base', 'blink', 'body', 'doctype', 'embed', 'font', 'form', 'frame', 'frameset', 'html', 'iframe', 'input', 'marquee', 'meta', 'noscript', 'object', 'param', 'script', 'style'
-  'strip_html_tags.tags'=> [ 'base', 'blink', 'body', 'doctype', 'embed', 'font', 'form', 'frame', 'frameset', 'html', 'iframe', 'input', 'marquee', 'meta', 'noscript', 'object', 'param', 'script', 'style'],
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Life
+    |--------------------------------------------------------------------------
+    |
+    | Life of cache, in seconds
+    |
+    */
+    'cache.life'               => 3600,
 
-  /*
-  |--------------------------------------------------------------------------
-  | Strip Attributes Disabled
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-    'strip_attribute.disabled'=> false,
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Disabled
+    |--------------------------------------------------------------------------
+    |
+    | Whether to disable the cache.
+    |
+    */
+    'cache.disabled'           => false,
 
-  /*
-  |--------------------------------------------------------------------------
-  | Striped Attributes Tags
-  |--------------------------------------------------------------------------
-  |
-  |
-  |
-  */
-  // 'bgsound', 'class', 'expr', 'id', 'style', 'onclick', 'onerror', 'onfinish', 'onmouseover', 'onmouseout', 'onfocus', 'onblur', 'lowsrc', 'dynsrc'
-    'strip_attributes.tags'=> [ 'bgsound', 'class', 'expr', 'id', 'style', 'onclick', 'onerror', 'onfinish', 'onmouseover', 'onmouseout', 'onfocus', 'onblur', 'lowsrc', 'dynsrc'],
+    /*
+    |--------------------------------------------------------------------------
+    | Disable Check for SSL certificates (enable for self signed certificates)
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    'ssl_check.disabled'       => false,
+    /*
+    |--------------------------------------------------------------------------
+    | Strip Html Tags Disabled
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    'strip_html_tags.disabled' => false,
 
-  /*
-  |--------------------------------------------------------------------------
-  | CURL Options
-  |--------------------------------------------------------------------------
-  |
-  | Array of CURL options (see curl_setopt())
-  | Set to null to disable
-  |
-  */
-  'curl.options' => null,
-  
-  'curl.timeout' => null,
+    /*
+    |--------------------------------------------------------------------------
+    | Stripped Html Tags
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    'strip_html_tags.tags'     => [
+        'base', 'blink', 'body', 'doctype', 'embed', 'font', 'form', 'frame', 'frameset', 'html', 'iframe', 'input',
+        'marquee', 'meta', 'noscript', 'object', 'param', 'script', 'style',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Strip Attributes Disabled
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    'strip_attribute.disabled' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stripped Attributes Tags
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    */
+    'strip_attributes.tags'    => [
+        'bgsound', 'class', 'expr', 'id', 'style', 'onclick', 'onerror', 'onfinish', 'onmouseover', 'onmouseout',
+        'onfocus', 'onblur', 'lowsrc', 'dynsrc',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | CURL Options
+    |--------------------------------------------------------------------------
+    |
+    | Array of CURL options (see curl_setopt())
+    | Set to null to disable
+    |
+    */
+    'curl.options'             => null,
+
+    'curl.timeout' => null,
 
 ];
